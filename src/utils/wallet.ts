58666fc2cf7fc345394227c1f9f4ac8d6bf04a1c
shendel <im_shendel@inbox.ru>
2021-12-03 18:40:09 +0300

    update matic build

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -10,7 +10,8 @@ import { nodes } from './getRpcUrl'
 export const setupNetwork = async () => {
   const provider = window.ethereum
   if (provider) {
-    const chainId = parseInt(process.env.REACT_APP_CHAIN_ID, 10)
+    // @ts-ignore
+    const chainId = parseInt(window.SO_LotteryConfig.chainId, 10)
     try {
       await provider.request({
         method: 'wallet_addEthereumChain',
